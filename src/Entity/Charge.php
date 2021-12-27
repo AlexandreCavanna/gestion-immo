7cdbe0c5a9d539d8b238c685162abe890d8033fa
Alexandre Cavanna <alexandre.cavanna.pro@gmail.com>
2021-12-27 16:55:41 +0100

    add housing & charge entities

---
+++ src/Entity/Charge.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChargeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChargeRepository::class)]
+class Charge
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'float', nullable: true)]
+    private $boilerCharge;
+
+    #[ORM\Column(type: 'float')]
+    private $electricityCharge;
+
+    #[ORM\Column(type: 'float', nullable: true)]
+    private $maintenanceCharge;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBoilerCharge(): ?float
+    {
+        return $this->boilerCharge;
+    }
+
+    public function setBoilerCharge(?float $boilerCharge): self
+    {
+        $this->boilerCharge = $boilerCharge;
+
+        return $this;
+    }
+
+    public function getElectricityCharge(): ?float
+    {
+        return $this->electricityCharge;
+    }
+
+    public function setElectricityCharge(float $electricityCharge): self
+    {
+        $this->electricityCharge = $electricityCharge;
+
+        return $this;
+    }
+
+    public function getMaintenanceCharge(): ?float
+    {
+        return $this->maintenanceCharge;
+    }
+
+    public function setMaintenanceCharge(?float $maintenanceCharge): self
+    {
+        $this->maintenanceCharge = $maintenanceCharge;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Housing.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HousingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HousingRepository::class)]
+class Housing
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $lot;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLot(): ?string
+    {
+        return $this->lot;
+    }
+
+    public function setLot(string $lot): self
+    {
+        $this->lot = $lot;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
